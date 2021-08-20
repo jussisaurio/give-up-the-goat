7cb21261949b0d57912b51c97640744e57b9d80e
jussisaurio <jussi.saurio@gmail.com>
2021-08-20 21:06:29 +0300

    Refactor Card

--- frontend/Card.tsx
+++ frontend/Card.tsx
@@ -63,120 +63,95 @@ export const PlayingCard = ({
   disabled = false,
   onClick
 }: Props) => {
-  if (card.face === "DOWN") {
-    return (
-      <BaseCard
-        id={id}
-        onClick={onClick}
-        animation={animation}
-        disabled={disabled}
-        selectable={selectable}
-        className={className}
-        style={{
+  const baseProps = {
+    id,
+    onClick,
+    animation,
+    disabled,
+    selectable,
+    className
+  };
+
+  const magnifyingGlass = (
+    <span style={{ color: "white", fontSize: "1.2rem" }}>🔍</span>
+  );
+  const goat = (
+    <img
+      style={{ width: "75%", height: "auto" }}
+      src="/assets/Stylized-Goat-Line-Art.svg"
+    ></img>
+  );
+  const civilian = (
+    <img
+      style={{ width: "75%", height: "auto" }}
+      src="/assets/Neutral.svg"
+    ></img>
+  );
+
+  const [styles, children] = (() => {
+    if (card.face === "DOWN") {
+      return [
+        {
           backgroundColor: "#222",
           ...style
-        }}
-      >
-        <span style={{ color: "white", fontSize: "1.2rem" }}>🔍</span>
-      </BaseCard>
-    );
-  }
+        },
+        magnifyingGlass
+      ];
+    } else if (card.card.type === "dual") {
+      const [firstColor, secondColor] = card.card.colors.map(
+        mapPlayerColorToUIColor
+      );
 
-  if (card.card.type === "dual") {
-    const [firstColor, secondColor] = card.card.colors.map(
-      mapPlayerColorToUIColor
-    );
-    return (
-      <BaseCard
-        id={id}
-        onClick={onClick}
-        selectable={selectable}
-        animation={animation}
-        disabled={disabled}
-        className={className}
-        style={{
+      return [
+        {
           backgroundImage: `linear-gradient(115deg, ${firstColor} 50%, ${secondColor} 50%)`,
           ...style
-        }}
-      >
-        <img
-          style={{ width: "75%", height: "auto" }}
-          src="/assets/Stylized-Goat-Line-Art.svg"
-        ></img>
-      </BaseCard>
-    );
-  } else if (card.card.type === "single") {
-    return (
-      <BaseCard
-        id={id}
-        onClick={onClick}
-        selectable={selectable}
-        animation={animation}
-        disabled={disabled}
-        className={className}
-        style={{
+        },
+        goat
+      ];
+    } else if (card.card.type === "single") {
+      return [
+        {
           backgroundColor: mapPlayerColorToUIColor(card.card.color),
           ...style
-        }}
-      >
-        <img
-          style={{ width: "75%", height: "auto" }}
-          src="/assets/Stylized-Goat-Line-Art.svg"
-        ></img>
-      </BaseCard>
-    );
-  } else if (card.card.type === "neutral") {
-    return (
-      <BaseCard
-        id={id}
-        onClick={onClick}
-        selectable={selectable}
-        animation={animation}
-        disabled={disabled}
-        className={className}
-        style={{
+        },
+        goat
+      ];
+    } else if (card.card.type === "neutral") {
+      return [
+        {
           backgroundColor: "grey",
           ...style
-        }}
-      >
-        <img
-          style={{ width: "75%", height: "auto" }}
-          src="/assets/Neutral.svg"
-        ></img>
-      </BaseCard>
-    );
-  }
+        },
+        civilian
+      ];
+    }
+
+    return [
+      {
+        background: `linear-gradient(
+      90deg,
+      rgba(255, 0, 0, 1) 0%,
+      rgba(255, 154, 0, 1) 10%,
+      rgba(208, 222, 33, 1) 20%,
+      rgba(79, 220, 74, 1) 30%,
+      rgba(63, 218, 216, 1) 40%,
+      rgba(47, 201, 226, 1) 50%,
+      rgba(28, 127, 238, 1) 60%,
+      rgba(95, 21, 242, 1) 70%,
+      rgba(186, 12, 248, 1) 80%,
+      rgba(251, 7, 217, 1) 90%,
+      rgba(255, 0, 0, 1) 100%
+  )`,
+        ...style
+      },
+      goat
+    ];
+  })();
 
   return (
-    <BaseCard
-      id={id}
-      onClick={onClick}
-      selectable={selectable}
-      animation={animation}
-      disabled={disabled}
-      className={className}
-      style={{
-        background: `linear-gradient(
-        90deg,
-        rgba(255, 0, 0, 1) 0%,
-        rgba(255, 154, 0, 1) 10%,
-        rgba(208, 222, 33, 1) 20%,
-        rgba(79, 220, 74, 1) 30%,
-        rgba(63, 218, 216, 1) 40%,
-        rgba(47, 201, 226, 1) 50%,
-        rgba(28, 127, 238, 1) 60%,
-        rgba(95, 21, 242, 1) 70%,
-        rgba(186, 12, 248, 1) 80%,
-        rgba(251, 7, 217, 1) 90%,
-        rgba(255, 0, 0, 1) 100%
-    )`,
-        ...style
-      }}
-    >
-      <img
-        style={{ width: "75%", height: "auto" }}
-        src="/assets/Stylized-Goat-Line-Art.svg"
-      ></img>
+    <BaseCard {...baseProps} style={styles}>
+      {children}
     </BaseCard>
   );
 };
